089d2235cf434d5d07cd1309c03eade1b85bc7ca
dokeun_oh <ohdoking@softwareinlfie.com>
2016-05-04 16:29:23 +0900

    main, login 페이지 로그 추가

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 var React = require('react-native');
 
+/*
+ *	사용하려는 ReactNative Module을 아래와 같이 불러온다. 
+ */
 var {
 	Navigator,
 	StyleSheet,
@@ -7,24 +10,49 @@ var {
 	View
 } = React;
 
+/*
+	Custom ReactNative Module을 불러온다.
+ */
 var Login = require('./pages/login');
 var Channels = require('./pages/channels');
 var Detail = require('./pages/detail');
+var WebView = require('./pages/web-view');
 
-
+/*
+	Navigator으로 조작할 View의 목록을 정의	
+ */
 var ROUTES = {
 	login: Login,
-    channels: Channels
     channels: Channels,
     detail: Detail,
+    webview: WebView
 };
 
+/*
+	Main React 컴포넌트를 생성
+ */
 var Main = React.createClass({
+	/*
+		{...route.passProps}를 추가해줘야 내부 컴포넌트에서 passProp로 데이터를 전달 가능
+		변수에 require로 받아온 모듈의 이름으로 사용 할 수 있다.
+	 */	
 	renderScene: function(route, navigator){
 		var Component = ROUTES[route.name];
-		return <Component route={route} navigator={navigator} />;
+
 		return <Component route={route} navigator={navigator} {...route.passProps} />;
 	},
+
+	/*
+		화면을 뿌려주는 메서드
+
+		Navigator
+		
+		Navigator 는 앱에서 장면들끼리 전환시킬수 있도록 도와주는 컴포넌트이다.
+		style은 말 그대로 컴포넌트의 스타일을 정의해준다.
+		initialRoute는 초기화시켜줄 화면을 정의하는 부분이다.
+		renderScene은 라우트에서 주어진 장면을 랜딩하여 사용할 수 있도록 정의한다.
+		configScene은 장면이 변할때 동작을 정의해준다.
+	 */
 	render: function(){
 		return(         
 			<Navigator
@@ -33,18 +61,23 @@ var Main = React.createClass({
 				renderScene={this.renderScene}
 				configureScene={
 					()=> {
-						return Navigator.SceneConfigs.FloatFromRight
+						return Navigator.SceneConfigs.FadeAndroid
 					}
 				}
 			/>
 		);
 	}
 });
-
+/*
+	Style을 정의한다.
+ */
 var styles = StyleSheet.create({
 	container: {
 		flex: 1
 	},	
 });
 
+/*
+	다른곳에서 사용 할 수 있도록 모듈화 시켜준다.
+ */
 module.exports = Main;
--- src/pages/login.js
+++ src/pages/login.js
@@ -11,13 +11,36 @@ var {
 
 
 var Login = React.createClass({
+	/*
+		state를 초기화 하는 곳이다.
+	 */
 	getInitialState: function(){
-		return{
+	    this.state = {
 			username: ''
-		};
+		}
+		return this.state;
 	},
-	render: function(){
+	/*
+		View
 
+		View는 대부분의 UI 가 만들어지는 근본적인 컴포넌트이다.
+		View는 flexbox, style, 터치동작, 접근 컨트롤 디자인응의 레이아웃을 지원한다.
+		(View is a container that supports layout with flexbox, style, some touch handling, and accessibility controls, and is designed to be nested inside other views and to have 0 to many children of any type.)
+		View는 2개 이상의 자식 뷰를 포함할수 있다.
+
+		TextInput
+
+		TextInput은 말그대로 글을 넣을 수 있는 input 컴포넌트이다.
+		onChangeText은 텍스트가 변할때마다 호출하여 실행된다.
+
+		TouchableHighlight
+
+		TouchableHighlight는 View가 터치에 반응 할 수 있도록 감싸 만들어주는 컴포넌트이다.
+		TouchableHighlight는 오직 하나의 자식 뷰만 지원 가능하다!
+		underlayColor는 터치가 되었을때 변할 색을 정의해준다.
+
+	 */
+	render: function(){
 		return (
 			<View style={styles.container}>
 				<View style={styles.loginContainer}>
@@ -41,6 +64,17 @@ var Login = React.createClass({
 			</View>
 		);
 	},
+
+	/*
+		함수를 이와 같이 정의하여 View 컴포넌트에서 사용 할 수 있다.
+
+		 this.props.navigator.push 를 사용하면 navigator의 Route에 등록된 다른 화면으로 전환 가능하다.
+		 name : [Route에 등록된 View 이름]
+
+		 ToastAndroid 는 Toast 컴포넌트이다. 
+		 안드로이드에서 사용하는 Toast와 정의하는 방법이 비슷하다.
+
+	 */
 	onPress: function() {
 	    console.log(this.state.username);
 	    if(this.state.username == "ohdoking"){
